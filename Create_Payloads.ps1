42a0a5bdd165cae91bc632d9126a21e7868b1f83
ofelman <61465999+ofelman@users.noreply.github.com>
2020-11-06 13:04:34 -0500

    Add files via upload

--- Create_Payloads.ps1
+++ Create_Payloads.ps1
@@ -28,6 +28,8 @@
 Param(
     [Parameter(Mandatory=$false)] [switch]$spm,
     [Parameter(Mandatory=$false)] [switch]$ebam, 
+    [Parameter(Mandatory=$false)] [switch]$provision=$true, 
+    [Parameter(Mandatory=$false)] [switch]$deprovision=$false, 
     [Parameter(Mandatory=$false)] [string]$CertsPath = $PSScriptRoot+'\certs',
     [Parameter(Mandatory=$false)] [string]$PayloadsPath = $PSScriptRoot+'\payloads',
     [Parameter(Mandatory=$false, ValueFromRemainingArguments)] [Switch]$help
@@ -38,9 +40,11 @@ Param(
 
 if ( ($PSBoundParameters.count -eq 0) -or ($help) ) {
     "Run script with options >"
-    "`tCreate_EBAM_Payloads.ps1 [-SPM] | [-EBAM] [-CertsPath <path>] [-PayloadsPath <path>] [-h|-help]"
-    "`tCreate_EBAM_Payloads.ps1 -SPM                  # To create only SPM payloads"
-    "`tCreate_EBAM_Payloads.ps1 -SPM -EBAM            # Create all payloads"
+    "`n`tCreate_Payloads.ps1 [[-SPM]|[-EBAM]] [-provision|-deprovision] [-CertsPath <path>] [-PayloadsPath <path>] [-h|-help]"
+    "`n`tCreate_Payloads.ps1 -SPM                   # To create SPM provision payloads (defaults to -Provision)"
+    "`tCreate_Payloads.ps1 -SPM -deprovision      # To create SPM deprovision payloads"
+    "`tCreate_Payloads.ps1 -EBAM                  # Create all payloads (defaults to -Provision)"
+    "`tCreate_Payloads.ps1 -EBAM -deprovision     # Create all payloads"
     "`n-CertsPath defaults to .\certs"
     "-PayloadsPath defaults to .\payloads"
     exit
@@ -49,6 +53,10 @@ if ( ($PSBoundParameters.count -eq 0) -or ($help) ) {
 if ( ($spm -and $ebam) -or (-not $spm -and -not $ebam) ) {
     'Please select either -spm or -ebam option'
     exit
+}
+write-host $deprovision
+if ( $deprovision ) {
+    $provision = $false
 }
 #=====================================================================================
 $Path = $PSScriptRoot
@@ -91,21 +99,25 @@ if ( $spm ) {
     # Create the Endorsement Payload
     # =========================================
 
-    write-Output 'Create the Endorsement Provisioning Payload: EKpayload.dat'
-    if ( Get-HPBIOSSetupPasswordIsSet ) {
-        New-HPSecurePlatformEndorsementKeyProvisioningPayload -EndorsementKeyFile $EKpfx -EndorsementKeyPassword $EKCertPwd -BIOSPassword $BIOSPwd -OutputFile EKpayload.dat
-    } else {
-        New-HPSecurePlatformEndorsementKeyProvisioningPayload -EndorsementKeyFile $EKpfx -EndorsementKeyPassword $EKCertPwd -OutputFile EKpayload.dat
-    }
-    write-Output 'Create the Endorsement Key Deprovisioning Payload: EKDepropayload.dat'
-    New-HPSecurePlatformDeprovisioningPayload -EndorsementKeyFile $EKpfx -EndorsementKeyPassword $EKCertPwd -OutputFile EKDepropayload.dat
+    if ( $provision ) {
+        write-Output 'Create the Endorsement Provisioning Payload: EKpayload.dat'
+        if ( Get-HPBIOSSetupPasswordIsSet ) {
+            New-HPSecurePlatformEndorsementKeyProvisioningPayload -EndorsementKeyFile $EKpfx -EndorsementKeyPassword $EKCertPwd -BIOSPassword $BIOSPwd -OutputFile EKpayload.dat
+        } else {
+            New-HPSecurePlatformEndorsementKeyProvisioningPayload -EndorsementKeyFile $EKpfx -EndorsementKeyPassword $EKCertPwd -OutputFile EKpayload.dat
+        }
+        # =========================================
+        # Create the Signing Keys and Payload
+        # =========================================
+        write-Output 'Create the Signing Key Provisioning Payload: SKpayload.dat'
+        New-HPSecurePlatformSigningKeyProvisioningPayload -EndorsementKeyFile $EKpfx -EndorsementKeyPassword $EKCertPwd -SigningKeyFile $SKpfx -SigningKeyPassword $SKCertPwd -OutputFile SKpayload.dat
+    } # if ( $provision )
 
-    # =========================================
-    # Create the Signing Keys and Payload
-    # =========================================
+    if ( $deprovision ) {
+        write-Output 'Create the Endorsement Key Deprovisioning Payload: EKDepropayload.dat'
+        New-HPSecurePlatformDeprovisioningPayload -EndorsementKeyFile $EKpfx -EndorsementKeyPassword $EKCertPwd -OutputFile EKDepropayload.dat
+    } # if ( $deprovision )
 
-    write-Output 'Create the Signing Key Provisioning Payload: SKpayload.dat'
-    New-HPSecurePlatformSigningKeyProvisioningPayload -EndorsementKeyFile $EKpfx -EndorsementKeyPassword $EKCertPwd -SigningKeyFile $SKpfx -SigningKeyPassword $SKCertPwd -OutputFile SKpayload.dat
 } # if ( $spm )
 
 #=====================================================================================
@@ -122,15 +134,20 @@ if ( $ebam ) {
     # Create EBAM Enable & Disable Payloads
     # - for remote AND F10 BIOS access
     # =========================================
-    write-Output 'Create the Sure Admin Enable Provisioning Payload: SAEnablepayload.dat'
-    New-HPSureAdminEnablePayload -SigningKeyFile $SKpfx -SigningKeyPassword $SKCertPwd -OutputFile SAEnablepayload.dat 
-    write-Output 'Create the Sure Admin Disable Deprovisioning Payload: SADeproPayload.dat'
-    New-HPSureAdminDisablePayload -SigningKeyFile $SKpfx -SigningKeyPassword $SKCertPwd -OutputFile SADisablePayload.dat 
+    if ( $provision ) {
+        write-Output 'Create the Sure Admin Enable Provisioning Payload: SAEnablepayload.dat'
+        New-HPSureAdminEnablePayload -SigningKeyFile $SKpfx -SigningKeyPassword $SKCertPwd -OutputFile SAEnablepayload.dat 
+        
+        write-Output 'Create the Sure Admin LAK Provisioning Payload: LAKpayload.dat'
+        New-HPSureAdminLocalAccessKeyProvisioningPayload -SigningKeyFile $SKpfx -SigningKeyPassword $SKCertPwd -LocalAccessKeyFile $LAKpfx -OutputFile LAKpayload.dat 
+    }
+    if ( $deprovision ) {
+        write-Output 'Create the Sure Admin Disable Deprovisioning Payload: SADeproPayload.dat'
+        New-HPSureAdminDisablePayload -SigningKeyFile $SKpfx -SigningKeyPassword $SKCertPwd -OutputFile SADisablePayload.dat 
 
-    write-Output 'Create the Sure Admin LAK Provisioning Payload: LAKpayload.dat'
-    New-HPSureAdminLocalAccessKeyProvisioningPayload -SigningKeyFile $SKpfx -SigningKeyPassword $SKCertPwd -LocalAccessKeyFile $LAKpfx -OutputFile LAKpayload.dat 
-    write-Output 'Create the Sure Admin LAK Deprovisioning Payload: LAKDepropayload.dat'
-    New-HPSureAdminBIOSSettingValuePayload -SigningKeyFile $SKpfx -SigningKeyPassword $SKCertPwd -Name "Enhanced BIOS Authentication Mode Local Access Key 1" -Value "" -OutputFile LAKDepropayload.dat
+        write-Output 'Create the Sure Admin LAK Deprovisioning Payload: LAKDepropayload.dat'
+        New-HPSureAdminBIOSSettingValuePayload -SigningKeyFile $SKpfx -SigningKeyPassword $SKCertPwd -Name "Enhanced BIOS Authentication Mode Local Access Key 1" -Value "" -OutputFile LAKDepropayload.dat
+    }
 
 } # if ( $ebam )
 
